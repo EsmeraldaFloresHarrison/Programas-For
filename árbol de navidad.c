c265ca3538264c4f16ddc7c0bb9583a53d44df84
EsmeraldaFloresHarrison <124409538+EsmeraldaFloresHarrison@users.noreply.github.com>
2023-03-01 08:02:54 -0600

    Create árbol de navidad.c

---
+++ árbol de navidad.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int altura, espacios, asteriscos;
+    
+    printf("Introduce la altura del árbol: ");
+    scanf("%d", &altura);
+    
+    espacios = altura - 1;
+    asteriscos = 1;
+    
+    for (int i = 0; i < altura; i++) {
+        for (int j = 0; j < espacios; j++) {
+            printf(" ");
+        }
+        
+        for (int j = 0; j < asteriscos; j++) {
+            printf("*");
+        }
+        
+        printf("\n");
+        
+        espacios--;
+        asteriscos += 2;
+    }
+    
+    // Tronco del árbol
+    for (int i = 0; i < altura - 1; i++) {
+        printf(" ");
+    }
+    printf("|\n");
+    for (int i = 0; i < altura - 2; i++) {
+        printf(" ");
+    }
+    printf("|||\n");
+    
+    return 0;
+}
