872480d0bd48518280b9b9cfb7311b9611ab2286
EsmeraldaFloresHarrison <124409538+EsmeraldaFloresHarrison@users.noreply.github.com>
2023-03-02 12:17:39 -0600

    Update números primo.c

--- números primo.c
+++ números primo.c
@@ -1,18 +1,36 @@
+/*Este programa iImprime la serie de números primos del 1 al 50*/
+
 #include <stdio.h>
 
+
 int main() {
+
    int i, j, es_primo;
+   
    for(i=1; i<=50; i++) {
+   
       es_primo = 1;
+      
       for(j=2; j<=i/2; j++) {
+      
          if(i%j == 0) {
+         
             es_primo = 0;
+            
             break;
+
          }
+         
       }
+      
       if(es_primo == 1) {
+      
          printf("%d ", i);
+
       }
+      
    }
+   
    return 0;
+
 }
