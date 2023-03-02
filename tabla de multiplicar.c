739de0d63a4fee729091086adbcd28f20a45ee5e
EsmeraldaFloresHarrison <124409538+EsmeraldaFloresHarrison@users.noreply.github.com>
2023-03-02 12:15:51 -0600

    Update tabla de multiplicar.c

--- tabla de multiplicar.c
+++ tabla de multiplicar.c
@@ -1,12 +1,22 @@
+/*Este programa utiliza un ciclo for anidado para imprimir la tabla de multiplicar del 1 al 10*/
+
 #include <stdio.h>
 
 int main() {
+
    int i, j;
+   
    for(i=1; i<=10; i++) {
+   
       printf("\nTabla de multiplicar del %d:\n", i);
+      
       for(j=1; j<=10; j++) {
+      
          printf("%d x %d = %d\n", i, j, i*j);
+      
       }
+      
    }
+   
    return 0;
 }
